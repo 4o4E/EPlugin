3cc0f2c3616a3449691d1e2d0fdc75c0bb766bf4
4o4E <869951226@qq.com>
2022-06-30 23:14:27 +0800

    使用buildSrc管理依赖版本

---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,7 @@
+object Versions {
+    const val kotlin = "1.7.0"
+    const val spigot = "1.13.2-R0.1-SNAPSHOT"
+    const val group = "top.e404"
+    const val version = "1.0.1"
+}
+fun kotlinx(id: String, version: String) = "org.jetbrains.kotlinx:kotlinx-$id:$version"
