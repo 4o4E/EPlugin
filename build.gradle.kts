44a8b33b1b57a3ab4506875d67f9669bc97b118e
4o4E <869951226@qq.com>
2022-06-30 23:15:08 +0800

    使用buildSrc管理依赖版本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version Versions.kotlin
     `maven-publish`
     `java-library`
 }
 
-group = "top.e404"
-version = "1.0.0"
+group = Versions.group
+version = Versions.version
 
 repositories {
     // spigot
@@ -19,14 +19,14 @@ repositories {
 
 dependencies {
     // spigot
-    compileOnly("org.spigotmc:spigot-api:1.13.2-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:${Versions.spigot}")
     // bstats
     implementation("org.bstats:bstats-bukkit:3.0.0")
 
     // eplugin
-    testImplementation("top.e404:EPlugin:1.0.0")
+    testImplementation(rootProject)
     // spigot
-    testImplementation("org.spigotmc:spigot-api:1.13.2-R0.1-SNAPSHOT")
+    testImplementation("org.spigotmc:spigot-api:${Versions.spigot}")
 }
 
 tasks.withType<KotlinCompile> {
