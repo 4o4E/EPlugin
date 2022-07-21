6018e929a2a61cbbd928eb12bf6dca51db6ad816
4o4E <869951226@qq.com>
2022-07-21 15:50:14 +0800

    添加工具方法

--- src/main/kotlin/util/item.kt
+++ src/main/kotlin/util/item.kt
@@ -2,8 +2,10 @@
 
 package top.e404.eplugin.util
 
+import org.bukkit.Material
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
+import top.e404.eplugin.EPlugin.Companion.color
 
 var ItemStack.lore
     get() = itemMeta?.lore ?: emptyList()
@@ -21,4 +23,19 @@ fun ItemStack.editItemMeta(block: ItemMeta.() -> Unit) = apply {
     val im = itemMeta ?: ItemStack(type).itemMeta!!
     im.block()
     itemMeta = im
+}
+
+val emptyItem: ItemStack
+    get() = ItemStack(Material.AIR)
+
+fun buildItemStack(
+    type: Material,
+    amount: Int = 1,
+    name: String?,
+    lore: List<String>? = null,
+    block: ItemMeta.() -> Unit = {}
+) = ItemStack(type, amount).editItemMeta {
+    name?.let { setDisplayName(it.color()) }
+    lore?.let { list -> this.lore = list.map { it.color() } }
+    block()
 }
