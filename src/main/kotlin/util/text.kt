b511459b122fbdde06410f026be6c70993d1b6e7
4o4E <869951226@qq.com>
2022-07-21 15:49:16 +0800

    添加工具方法

--- src/main/kotlin/util/text.kt
+++ src/main/kotlin/util/text.kt
@@ -16,5 +16,55 @@ fun TextComponent.click(action: ClickEvent.Action, value: String) = apply {
     this.clickEvent = ClickEvent(action, value)
 }
 
+fun Collection<String>.toComponent(
+    prefix: TextComponent,
+    spacing: TextComponent,
+    suffix: TextComponent,
+    block: String.() -> TextComponent
+): MutableList<TextComponent> {
+    val iterator = iterator()
+    val list = ArrayList<TextComponent>(size * 2 + 1)
+    list.add(prefix)
+    while (true) {
+        val next = iterator.next()
+        list.add(next.block())
+        if (!iterator.hasNext()) break
+        list.add(spacing)
+    }
+    list.add(suffix)
+    return list
+}
+
+fun Collection<TextComponent>.toComponent(
+    prefix: TextComponent,
+    spacing: TextComponent,
+    suffix: TextComponent
+): MutableList<TextComponent> {
+    val iterator = iterator()
+    val list = ArrayList<TextComponent>(size * 2 + 1)
+    list.add(prefix)
+    while (true) {
+        val next = iterator.next()
+        list.add(next)
+        if (!iterator.hasNext()) break
+        list.add(spacing)
+    }
+    list.add(suffix)
+    return list
+}
+
+fun Collection<String>.toComponentArray(
+    prefix: TextComponent,
+    spacing: TextComponent,
+    suffix: TextComponent,
+    block: String.() -> TextComponent
+) = toComponent(prefix, spacing, suffix, block).toTypedArray()
+
+fun Collection<TextComponent>.toComponentArray(
+    prefix: TextComponent,
+    spacing: TextComponent,
+    suffix: TextComponent
+) = toComponent(prefix, spacing, suffix).toTypedArray()
+
 val Location.asString
     get() = "world: ${world?.name}, x: $blockX, y: $blockY, z: $blockZ"
