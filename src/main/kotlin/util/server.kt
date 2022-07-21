ea8317b48fd137cee679bb4f3e65e8444eb5b192
4o4E <869951226@qq.com>
2022-07-21 15:49:35 +0800

    修复服务器版本获取时的错误

--- src/main/kotlin/util/server.kt
+++ src/main/kotlin/util/server.kt
@@ -7,12 +7,12 @@ import top.e404.eplugin.update.EUpdater
 import java.util.regex.Pattern
 
 val mcVer by lazy {
-    val p = Pattern.compile("(?<major>\\d+)\\.(?<minor>\\d+)(\\.(?<revision>\\d+)?)-.*")
+    val p = Pattern.compile("(?<major>\\d+)\\.(?<minor>\\d+)(\\.(?<revision>\\d+))?-.*")
     val m = p.matcher(Bukkit.getBukkitVersion())
     if (!m.find()) return@lazy null
     EUpdater.Ver(
         m.group("major").toInt(),
         m.group("minor").toInt(),
-        m.group("revision").toIntOrNull() ?: 0
+        m.group("revision")?.toIntOrNull() ?: 0
     )
 }
