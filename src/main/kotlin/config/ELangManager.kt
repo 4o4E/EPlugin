9bde92c3d30096312378e4e0c6e3c14fbdf0aef9
4o4E <869951226@qq.com>
2022-07-21 15:50:31 +0800

    修改方法

--- src/main/kotlin/config/ELangManager.kt
+++ src/main/kotlin/config/ELangManager.kt
@@ -3,6 +3,7 @@
 package top.e404.eplugin.config
 
 import top.e404.eplugin.EPlugin
+import top.e404.eplugin.EPlugin.Companion.color
 import top.e404.eplugin.EPlugin.Companion.placeholder
 
 abstract class ELangManager(
@@ -13,7 +14,9 @@ abstract class ELangManager(
     default = JarConfig(plugin, "lang.yml"),
     clearBeforeSave = false
 ) {
-    override operator fun get(path: String) = getOrSelf(path)
+    override operator fun get(path: String) = getOrSelf(path).color()
     operator fun get(path: String, vararg placeholder: Pair<String, Any?>) =
         config.getString(path)?.placeholder(*placeholder) ?: path
+
+    fun getList(path: String) = config.getStringList(path).map { it.color() }
 }
