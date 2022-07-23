0e636e7a2d76408849d605673dddcf6c9b948aa0
4o4E <869951226@qq.com>
2022-07-24 02:43:16 +0800

    修改hook manager模板

---
+++ src/main/kotlin/hook/EHookManager.kt
@@ -0,0 +1,25 @@
+package top.e404.eplugin.hook
+
+import top.e404.eplugin.EPlugin
+
+open class EHookManager(
+    val plugin: EPlugin,
+    vararg val hooks: EHook<*>,
+) : HashMap<String, EHook<*>>() {
+    override val size: Int = hooks.size
+
+    fun register() {
+        hooks.forEach { this[it.name] = it }
+        checkHooks()
+    }
+
+    fun addHook(name: String, hook: EHook<*>): Boolean {
+        if (name in this) return false
+        this[name] = hook
+        return true
+    }
+
+    fun delHook(name: String) = remove(name)
+
+    fun checkHooks() = values.forEach { it.checkHook() }
+}
