4c65a77af2788900be55d3b8f28d219c921f95be
4o4E <869951226@qq.com>
2022-07-24 02:40:46 +0800

    升级eplugin版本

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,6 +2,15 @@ object Versions {
     const val kotlin = "1.7.0"
     const val spigot = "1.13.2-R0.1-SNAPSHOT"
     const val group = "top.e404"
-    const val version = "1.0.2"
+    const val version = "1.0.3"
 }
+
+object Hooks {
+    const val slimefun = "RC-30"
+    const val mmoitems = "6.7.3"
+    const val mythicLib = "1.3.1"
+    const val worldguard = "7.0.8-SNAPSHOT"
+    const val bentobox = "1.20.1-SNAPSHOT"
+}
+
 fun kotlinx(id: String, version: String) = "org.jetbrains.kotlinx:kotlinx-$id:$version"
