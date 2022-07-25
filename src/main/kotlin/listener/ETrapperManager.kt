33d69833b0618dbc20fadcbf0651a4b7024e75a3
4o4E <869951226@qq.com>
2022-07-25 23:34:43 +0800

    添加hasTrapper方法

--- src/main/kotlin/listener/ETrapperManager.kt
+++ src/main/kotlin/listener/ETrapperManager.kt
@@ -1,5 +1,6 @@
 package top.e404.eplugin.listener
 
+import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.player.AsyncPlayerChatEvent
 import top.e404.eplugin.EPlugin
@@ -10,11 +11,13 @@ open class ETrapperManager(override val plugin: EPlugin) : EListener(plugin) {
 
     fun addTrapper(trapper: EChatTrapper) = trappers.add(trapper)
     fun cancelTrapper(trapper: EChatTrapper) = trappers.remove(trapper)
+    fun hasTrapper(p: Player) = trappers.any { it.target == p }
 
     @EventHandler
     fun AsyncPlayerChatEvent.onEvent() {
         for (trapper in trappers) {
             if (trapper.target != player) continue
+            isCancelled = true
             if (trapper.onChat(message)) trappers.remove(trapper)
             return
         }
