f08e98b15d0960e1957bb50774804ecd141cb9fa
4o4E <869951226@qq.com>
2022-07-27 01:23:37 +0800

    -

---
+++ eplugin-hook-itemrepo/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    `maven-publish`
+    `java-library`
+}
+
+group = Versions.group
+version = Versions.version
+
+repositories {
+    // spigot
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    // spigot
+    compileOnly("org.spigotmc:spigot-api:${Versions.spigot}")
+    // itemrepo
+    compileOnly("top.e404:item-repo:${Hooks.itemrepo}")
+    // eplugin
+    implementation(rootProject)
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+afterEvaluate {
+    publishing.publications.create<MavenPublication>("java") {
+        from(components["kotlin"])
+        artifact(tasks.getByName("sourcesJar"))
+        artifact(tasks.getByName("javadocJar"))
+        artifactId = "eplugin-hook-itemrepo"
+        groupId = Versions.group
+        version = Versions.version
+    }
+}
