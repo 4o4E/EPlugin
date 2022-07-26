db364d15a6c58cea551206cd56fe21b739b7b121
4o4E <869951226@qq.com>
2022-07-27 01:21:57 +0800

    修改EHook使用JavaPlugin作为泛型

--- src/main/kotlin/hook/EHook.kt
+++ src/main/kotlin/hook/EHook.kt
@@ -1,10 +1,10 @@
 package top.e404.eplugin.hook
 
 import org.bukkit.Bukkit
-import org.bukkit.plugin.Plugin
+import org.bukkit.plugin.java.JavaPlugin
 import top.e404.eplugin.EPlugin
 
-abstract class EHook<T : Plugin>(
+abstract class EHook<T : JavaPlugin>(
     open val plugin: EPlugin,
     open val name: String,
 ) {
