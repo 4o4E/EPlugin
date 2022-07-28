3ab8b5da21473b775ef52e3fd1b9375f416b3e29
4o4E <869951226@qq.com>
2022-07-28 19:41:15 +0800

    添加给予玩家物品的工具方法

--- src/main/kotlin/util/item.kt
+++ src/main/kotlin/util/item.kt
@@ -3,6 +3,7 @@
 package top.e404.eplugin.util
 
 import org.bukkit.Material
+import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
 import org.bukkit.inventory.meta.ItemMeta
 import top.e404.eplugin.EPlugin.Companion.color
@@ -38,4 +39,51 @@ fun buildItemStack(
     name?.let { setDisplayName(it.color()) }
     lore?.let { list -> this.lore = list.map { it.color() } }
     block()
+}
+
+/**
+ * 给予玩家物品, 自动堆叠
+ *
+ * @param item 给予的物品
+ * @return 若背包已满则掉落并返回false
+ */
+fun Player.giveStickItem(
+    item: ItemStack,
+): Boolean {
+    val maxStackSize = item.maxStackSize
+    // 先检查可堆叠
+    for ((index, invItem) in inventory.withIndex()) {
+        // 背包格子为空 || 物品不相同
+        if (invItem == null || !item.isSimilar(invItem)) continue
+        val stickAmount = maxStackSize - invItem.amount // 可以堆叠的数量
+        // 直接堆叠在物品上
+        if (stickAmount >= item.amount) {
+            invItem.amount += item.amount
+            inventory.setItem(index, invItem)
+            return true
+        }
+        // 有溢出
+        item.amount -= stickAmount
+        invItem.amount += stickAmount
+        inventory.setItem(index, invItem)
+    }
+    return giveItem(item)
+}
+
+/**
+ * 给予玩家物品, 不检查堆叠
+ *
+ * @param item 给予的物品
+ * @return 若背包已满则掉落并返回false
+ */
+fun Player.giveItem(
+    item: ItemStack,
+): Boolean {
+    val firstEmpty = inventory.firstEmpty()
+    if (firstEmpty == -1) {
+        world.dropItem(location, item)
+        return false
+    }
+    inventory.setItem(firstEmpty, item)
+    return true
 }
