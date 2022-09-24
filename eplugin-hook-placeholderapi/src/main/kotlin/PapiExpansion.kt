7e06e4ce0a65cc5797b69999628a8c3adbd74522
4o4E <869951226@qq.com>
2022-09-24 15:51:34 +0800

    添加papi扩展模板

---
+++ eplugin-hook-placeholderapi/src/main/kotlin/PapiExpansion.kt
@@ -0,0 +1,15 @@
+package top.e404.eplugin.hook.placeholderapi
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion
+import top.e404.eplugin.EPlugin
+
+abstract class PapiExpansion(
+    val plugin: EPlugin,
+    val id: String,
+) : PlaceholderExpansion() {
+    override fun getIdentifier() = id
+    override fun getAuthor() = plugin.description.authors.firstOrNull() ?: "unknown"
+    override fun getVersion() = plugin.description.version
+    override fun persist() = true
+    override fun canRegister() = true
+}
