6c21b1868dcfcbd9c2631c25843ed8d829d9f6ca
4o4E <869951226@qq.com>
2022-10-03 23:34:35 +0800

    移除父项目中build.gradle.kts中无用的依赖和插件

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,6 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version Versions.kotlin
     kotlin("plugin.serialization") version Versions.kotlin
-    `maven-publish`
-    `java-library`
 }
 
 group = Versions.group
@@ -18,35 +16,6 @@ repositories {
     mavenLocal()
 }
 
-dependencies {
-    // spigot
-    compileOnly("org.spigotmc:spigot-api:${Versions.spigot}")
-    // bstats
-    implementation("org.bstats:bstats-bukkit:3.0.0")
-    implementation(kotlinx("serialization-core-jvm", "1.3.3"))
-
-    // eplugin
-    testImplementation(project(":eplugin-core"))
-    // spigot
-    testImplementation("org.spigotmc:spigot-api:${Versions.spigot}")
-}
-
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
-}
-
-java {
-    withJavadocJar()
-    withSourcesJar()
-}
-
-afterEvaluate {
-    publishing.publications.create<MavenPublication>("java") {
-        from(components["kotlin"])
-        artifact(tasks.getByName("sourcesJar"))
-        artifact(tasks.getByName("javadocJar"))
-        artifactId = "eplugin-core"
-        groupId = Versions.group
-        version = Versions.version
-    }
 }
