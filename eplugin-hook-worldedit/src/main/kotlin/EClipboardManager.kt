6eec3b0d481d83def03ec9e3473aa979b476d49e
4o4E <869951226@qq.com>
2022-10-06 22:40:21 +0800

    添加worldedit-hook

---
+++ eplugin-hook-worldedit/src/main/kotlin/EClipboardManager.kt
@@ -0,0 +1,29 @@
+package top.e404.eplugin.hook.worldedit
+
+import com.sk89q.worldedit.extent.clipboard.Clipboard
+import top.e404.eplugin.EPlugin
+import java.io.File
+
+open class EClipboardManager(
+    open val plugin: EPlugin,
+    open val folder: File,
+) {
+    val map = mutableMapOf<String, Clipboard>()
+    fun load() = folder.also {
+        if (it.isFile) it.delete()
+        it.mkdirs()
+    }.loadAsSchem()
+
+    private fun File.loadAsSchem() {
+        if (isFile) {
+            map[name] = loadSchem(this)
+            return
+        }
+        listFiles()?.forEach { it.loadAsSchem() }
+    }
+
+    operator fun get(name: String) = map[name]
+    operator fun set(name: String, clipboard: Clipboard) {
+        map[name] = clipboard
+    }
+}
--- eplugin-hook-worldedit/src/main/kotlin/WorldEditHook.kt
+++ eplugin-hook-worldedit/src/main/kotlin/WorldEditHook.kt
@@ -1,14 +1,34 @@
 package top.e404.eplugin.hook.worldedit
 
 import com.sk89q.worldedit.WorldEdit
-import com.sk89q.worldedit.bukkit.WorldEditPlugin
+import com.sk89q.worldedit.extent.clipboard.Clipboard
+import com.sk89q.worldedit.extent.clipboard.io.BuiltInClipboardFormat
+import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats
 import top.e404.eplugin.EPlugin
 import top.e404.eplugin.hook.EHook
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
 
 @Suppress("UNUSED")
 open class WorldEditHook(
     override val plugin: EPlugin,
-) : EHook<WorldEditPlugin>(plugin, "WorldEdit") {
+) : EHook(plugin, "WorldEdit") {
     val we: WorldEdit
         get() = WorldEdit.getInstance()
+}
+
+fun loadSchem(file: File) = FileInputStream(file).use { fis ->
+    ClipboardFormats.findByFile(file)!!
+        .getReader(fis)
+        .use { reader -> reader.read()!! }
+}
+
+fun saveSchem(file: File, clipboard: Clipboard) {
+    FileOutputStream(file).use { fos ->
+        BuiltInClipboardFormat.SPONGE_SCHEMATIC
+            .getWriter(fos)
+            .use { writer -> writer.write(clipboard) }
+    }
 }
