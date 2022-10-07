e287ecba02a3890678705d54cfc8b49249993dc6
4o4E <869951226@qq.com>
2022-10-07 16:31:16 +0800

    添加Clipboard工具方法

--- eplugin-hook-worldedit/src/main/kotlin/WorldEditHook.kt
+++ eplugin-hook-worldedit/src/main/kotlin/WorldEditHook.kt
@@ -1,9 +1,13 @@
 package top.e404.eplugin.hook.worldedit
 
+import com.sk89q.worldedit.EditSession
 import com.sk89q.worldedit.WorldEdit
 import com.sk89q.worldedit.extent.clipboard.Clipboard
 import com.sk89q.worldedit.extent.clipboard.io.BuiltInClipboardFormat
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats
+import com.sk89q.worldedit.function.operation.Operations
+import com.sk89q.worldedit.math.BlockVector3
+import com.sk89q.worldedit.session.ClipboardHolder
 import top.e404.eplugin.EPlugin
 import top.e404.eplugin.hook.EHook
 import java.io.File
@@ -25,10 +29,43 @@ fun loadSchem(file: File) = FileInputStream(file).use { fis ->
         .use { reader -> reader.read()!! }
 }
 
-fun saveSchem(file: File, clipboard: Clipboard) {
+fun saveSchem(
+    file: File,
+    clipboard: Clipboard
+) {
     FileOutputStream(file).use { fos ->
         BuiltInClipboardFormat.SPONGE_SCHEMATIC
             .getWriter(fos)
             .use { writer -> writer.write(clipboard) }
     }
 }
+
+/**
+ * 将剪切板粘贴到指定位置
+ */
+fun Clipboard.paste(
+    session: EditSession,
+    x: Int,
+    y: Int,
+    z: Int
+) = Operations.complete(
+    ClipboardHolder(this)
+        .createPaste(session)
+        .to(BlockVector3.at(x, y, z))
+        .build()
+)
+
+/**
+ * 将剪切板粘贴到指定位置, 剪切板xz中心坐标对应给的xz坐标, 剪切板y最低点对应给的z坐标
+ */
+fun Clipboard.pasteCenter(
+    session: EditSession,
+    x: Int,
+    y: Int,
+    z: Int
+) = paste(
+    session = session,
+    x = x + origin.x - (maximumPoint.x + minimumPoint.x) / 2,
+    y = y + origin.y - minimumPoint.y,
+    z = z + origin.z - (maximumPoint.z + minimumPoint.z) / 2
+)
