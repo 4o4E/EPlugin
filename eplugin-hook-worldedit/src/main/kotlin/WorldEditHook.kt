63ca36c75354be17bba601c9624ac11a18ed8c83
4o4E <869951226@qq.com>
2022-10-12 00:24:40 +0800

    添加clear方法清理区域

--- eplugin-hook-worldedit/src/main/kotlin/WorldEditHook.kt
+++ eplugin-hook-worldedit/src/main/kotlin/WorldEditHook.kt
@@ -2,12 +2,18 @@ package top.e404.eplugin.hook.worldedit
 
 import com.sk89q.worldedit.EditSession
 import com.sk89q.worldedit.WorldEdit
+import com.sk89q.worldedit.bukkit.BukkitAdapter
 import com.sk89q.worldedit.extent.clipboard.Clipboard
 import com.sk89q.worldedit.extent.clipboard.io.BuiltInClipboardFormat
 import com.sk89q.worldedit.extent.clipboard.io.ClipboardFormats
 import com.sk89q.worldedit.function.operation.Operations
 import com.sk89q.worldedit.math.BlockVector3
+import com.sk89q.worldedit.regions.CuboidRegion
 import com.sk89q.worldedit.session.ClipboardHolder
+import org.bukkit.Location
+import org.bukkit.Material
+import org.bukkit.World
+import org.bukkit.inventory.ItemStack
 import top.e404.eplugin.EPlugin
 import top.e404.eplugin.hook.EHook
 import java.io.File
@@ -21,6 +27,44 @@ open class WorldEditHook(
 ) : EHook(plugin, "WorldEdit") {
     val we: WorldEdit
         get() = WorldEdit.getInstance()
+
+    fun clear(
+        world: World,
+        l1: Location,
+        l2: Location
+    ) = clear(
+        world,
+        l1.blockX,
+        l1.blockY,
+        l1.blockZ,
+        l2.blockX,
+        l2.blockY,
+        l2.blockZ
+    )
+
+    fun clear(
+        world: World,
+        x1: Int,
+        y1: Int,
+        z1: Int,
+        x2: Int,
+        y2: Int,
+        z2: Int,
+    ) {
+        val weWorld = BukkitAdapter.adapt(world)
+        WorldEdit.getInstance().newEditSession(weWorld).apply {
+            blockChangeLimit = -1
+        }.use { session ->
+            session.setBlocks(
+                CuboidRegion(
+                    weWorld,
+                    BlockVector3.at(x1, y1, z1),
+                    BlockVector3.at(x2, y2, z2)
+                ),
+                BukkitAdapter.asBlockState(ItemStack(Material.AIR))
+            )
+        }
+    }
 }
 
 fun loadSchem(file: File) = FileInputStream(file).use { fis ->
