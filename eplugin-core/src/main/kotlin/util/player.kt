604afcc6e494d92edd39f6b5f71f850173e7ab58
4o4E <869951226@qq.com>
2022-10-16 23:44:02 +0800

    添加获取玩家脚下方块的工具方法

--- eplugin-core/src/main/kotlin/util/player.kt
+++ eplugin-core/src/main/kotlin/util/player.kt
@@ -5,6 +5,7 @@ package top.e404.eplugin.util
 import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
+import org.bukkit.util.Vector
 
 fun OfflinePlayer.uuid() = uniqueId.toString()
 fun OfflinePlayer.fixedUuid() = uniqueId.toString().replace("-", "")
@@ -14,3 +15,14 @@ fun getOnlineOp() = getOnline().filter { it.isOp }
 
 fun forEachOp(block: (Player) -> Unit) = getOnlineOp().forEach(block)
 fun forEachOnline(block: (Player) -> Unit) = getOnline().forEach(block)
+
+private val vectors = listOf(
+    Vector(0.3, -1.0, 0.3),
+    Vector(-0.3, -1.0, 0.3),
+    Vector(0.3, -1.0, -0.3),
+    Vector(-0.3, -1.0, -0.3),
+)
+
+fun Player.getBlockUnderFoot() = vectors.map { location.add(it).block }
+
+fun Player.isMidAir() = getBlockUnderFoot().all { it.isEmpty }
