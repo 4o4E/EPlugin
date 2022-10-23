136985408015e6e9b7dff2914fa4b5296f7ecd63
4o4E <869951226@qq.com>
2022-10-24 00:46:22 +0800

    添加mm工具方法

--- eplugin-hook-mythicmobs/src/main/kotlin/MythicMobsHook.kt
+++ eplugin-hook-mythicmobs/src/main/kotlin/MythicMobsHook.kt
@@ -2,7 +2,9 @@ package top.e404.eplugin.hook.mmoitems
 
 import io.lumine.mythic.api.items.ItemManager
 import io.lumine.mythic.api.volatilecode.handlers.VolatileItemHandler
+import io.lumine.mythic.bukkit.BukkitAPIHelper
 import io.lumine.mythic.bukkit.MythicBukkit
+import org.bukkit.Location
 import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import top.e404.eplugin.EPlugin
@@ -23,13 +25,20 @@ open class MythicMobsHook(
     val itemHandler: VolatileItemHandler
         get() = mm.volatileCodeHandler.itemHandler
 
+    val apiHelper: BukkitAPIHelper
+        get() = mm.apiHelper
+
     @ExperimentalStdlibApi
     fun getItem(name: String) = itemManager.getItem(name).getOrNull()
+
     fun getMmId(item: ItemStack) = itemHandler.getNBTData(item).let { tag ->
         if (tag.containsKey("MYTHIC_TYPE")) tag.getString("MYTHIC_TYPE")
         else null
     }
 
+    fun getMythicMob(type: String) = apiHelper.getMythicMob(type)
+
+    fun spawnMythicMob(type: String, location: Location) = apiHelper.spawnMythicMob(type, location)
 
     fun count(
         inv: Inventory,
