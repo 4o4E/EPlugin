824da19f187b2a64246ea7e99d89c3650126b9e3
4o4E <869951226@qq.com>
2022-10-24 00:34:56 +0800

    添加mi背包物品统计和拿取的工具方法

--- eplugin-hook-mmoitems/src/main/kotlin/MmoitemsHook.kt
+++ eplugin-hook-mmoitems/src/main/kotlin/MmoitemsHook.kt
@@ -3,9 +3,11 @@ package top.e404.eplugin.hook.mmoitems
 import io.lumine.mythic.lib.api.item.NBTItem
 import net.Indyuce.mmoitems.MMOItems
 import net.Indyuce.mmoitems.api.Type
+import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import top.e404.eplugin.EPlugin
 import top.e404.eplugin.hook.EHook
+import top.e404.eplugin.inventory.take
 
 @Suppress("UNUSED")
 open class MmoitemsHook(
@@ -17,4 +19,25 @@ open class MmoitemsHook(
     fun getItem(type: String, id: String) = MMOItems.plugin.getMMOItem(Type.get(type), id)
     fun getNbtItem(itemStack: ItemStack) = NBTItem.get(itemStack)!!
     fun NBTItem.getMiId() = getString("MMOITEMS_ITEM_ID")!!
+
+    fun count(
+        inv: Inventory,
+        type: String,
+        id: String
+    ) = inv.count { item ->
+        getNbtItem(item).let {
+            it.type == type && it.getString("MMOITEMS_ITEM_ID") == id
+        }
+    }
+
+    fun take(
+        inv: Inventory,
+        type: String,
+        id: String,
+        amount: Int
+    ) = inv.take(amount) { item ->
+        getNbtItem(item).let {
+            it.type == type && it.getString("MMOITEMS_ITEM_ID") == id
+        }
+    }
 }
