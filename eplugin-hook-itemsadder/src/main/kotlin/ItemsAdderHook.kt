afa483b612f1f646fe0decbd5ea40cb13aa0f6ce
4o4E <869951226@qq.com>
2022-10-24 00:34:50 +0800

    添加ia背包物品统计和拿取的工具方法

--- eplugin-hook-itemsadder/src/main/kotlin/ItemsAdderHook.kt
+++ eplugin-hook-itemsadder/src/main/kotlin/ItemsAdderHook.kt
@@ -3,9 +3,11 @@ package top.e404.eplugin.hook.bentobox
 import dev.lone.itemsadder.api.CustomStack
 import dev.lone.itemsadder.api.ItemsAdder
 import org.bukkit.Material
+import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 import top.e404.eplugin.EPlugin
 import top.e404.eplugin.hook.EHook
+import top.e404.eplugin.inventory.take
 
 @Suppress("UNUSED")
 open class ItemsAdderHook(
@@ -21,6 +23,25 @@ open class ItemsAdderHook(
     fun getAllNamespace() = mutableSetOf<String>().apply {
         ItemsAdder.getAllItems().forEach {
             add(it.namespace)
+        }
+    }
+
+    fun count(
+        inv: Inventory,
+        namespacedID: String,
+    ) = inv.count { item ->
+        getIaItemInfo(item).let {
+            it?.namespacedID == namespacedID
+        }
+    }
+
+    fun take(
+        inv: Inventory,
+        namespacedID: String,
+        amount: Int
+    ) = inv.take(amount) { item ->
+        getIaItemInfo(item).let {
+            it?.namespacedID == namespacedID
         }
     }
 }
