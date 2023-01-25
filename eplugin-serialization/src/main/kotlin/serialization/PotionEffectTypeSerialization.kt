3a029f63c2b8e7f6575ec9950c03362f05d90559
4o4E <869951226@qq.com>
2023-01-25 20:03:55 +0800

    添加PotionEffectType序列化器

---
+++ eplugin-serialization/src/main/kotlin/serialization/PotionEffectTypeSerialization.kt
@@ -0,0 +1,18 @@
+@file:Suppress("UNUSED")
+
+package top.e404.eplugin.config.serialization
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import org.bukkit.potion.PotionEffectType
+
+object PotionEffectTypeSerialization : KSerializer<PotionEffectType> {
+    override val descriptor = PrimitiveSerialDescriptor("top.e404.eplugin.config.serialization.PotionEffectTypeSerialization", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): PotionEffectType = PotionEffectType.getByName(decoder.decodeString())!!
+
+    override fun serialize(encoder: Encoder, value: PotionEffectType) = encoder.encodeString(value.name)
+}
