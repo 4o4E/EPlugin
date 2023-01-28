aeb8d10eaa5f8706e4134fd25da732bf9ce95d34
4o4E <869951226@qq.com>
2023-01-29 02:12:58 +0800

    添加ItemStackMinSerialization

---
+++ eplugin-serialization/src/main/kotlin/serialization/items.kt
@@ -0,0 +1,25 @@
+package top.e404.eplugin.config.serialization
+
+import com.google.gson.reflect.TypeToken
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import org.bukkit.inventory.ItemStack
+import top.e404.eplugin.util.gson
+import top.e404.eplugin.util.toJson
+
+object ItemStackMinSerialization : KSerializer<ItemStack> {
+    private val type by lazy { object : TypeToken<Map<String, Any>>() {}.type }
+    override val descriptor = PrimitiveSerialDescriptor("top.e404.eplugin.config.serialization.ItemStackMinSerialization", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): ItemStack {
+        val data = gson.fromJson<Map<String, Any>>(decoder.decodeString(), type)
+        return ItemStack.deserialize(data)
+    }
+
+    override fun serialize(encoder: Encoder, value: ItemStack) {
+        encoder.encodeString(value.serialize().toJson())
+    }
+}
