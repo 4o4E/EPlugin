48ef7ccb84c386f74a69a7c0371847a1a5876d15
4o4E <869951226@qq.com>
2023-01-29 02:11:38 +0800

    添加adyeshach的hook

---
+++ eplugin-hook-adyeshach/build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    `maven-publish`
+    `java-library`
+}
+
+group = Versions.group
+version = Versions.version
+
+repositories {
+    mavenLocal()
+    // spigot
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    // ady
+    maven("https://repo.tabooproject.org/repository/releases/")
+    mavenCentral()
+}
+
+dependencies {
+    // spigot
+    compileOnly("org.spigotmc:spigot-api:${Versions.spigot}")
+    // ady
+    compileOnly("ink.ptms.adyeshach:all:2.0.0-snapshot-10")
+    // eplugin
+    implementation(project(":eplugin-core"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+afterEvaluate {
+    publishing.publications.create<MavenPublication>("java") {
+        from(components["kotlin"])
+        artifact(tasks.getByName("sourcesJar"))
+        artifact(tasks.getByName("javadocJar"))
+        artifactId = project.name
+        groupId = Versions.group
+        version = Versions.version
+    }
+}
---
+++ eplugin-hook-adyeshach/src/main/kotlin/AdyeshachHook.kt
@@ -0,0 +1,101 @@
+package top.e404.eplugin.hook.ady
+
+import ink.ptms.adyeshach.core.*
+import ink.ptms.adyeshach.core.entity.manager.ManagerType
+import org.bukkit.entity.Player
+import top.e404.eplugin.EPlugin
+import top.e404.eplugin.hook.EHook
+
+@Suppress("UNUSED")
+open class AdyeshachHook(
+    override val plugin: EPlugin,
+) : EHook(plugin, "Adyeshach") {
+    val api: AdyeshachAPI
+        get() = Adyeshach.api()
+
+
+    /**
+     * 初始化玩家管理器
+     */
+    fun setupEntityManager(player: Player) = api.setupEntityManager(player)
+
+    /**
+     * 释放玩家管理器
+     */
+    fun releaseEntityManager(player: Player, async: Boolean = true) = api.releaseEntityManager(player, async)
+
+    /**
+     * 刷新玩家管理器（重新显示所有单位）
+     */
+    fun refreshEntityManager(player: Player) = api.refreshEntityManager(player)
+
+    /**
+     * 获取公共单位管理器
+     *
+     * @param type 容器类型
+     */
+    fun getPublicEntityManager(type: ManagerType = ManagerType.TEMPORARY) = api.getPublicEntityManager(type)
+
+    /**
+     * 获取私有单位管理器
+     *
+     * @param player 玩家
+     * @param type 容器类型
+     */
+    fun getPrivateEntityManager(player: Player, type: ManagerType = ManagerType.TEMPORARY) = api.getPrivateEntityManager(player, type)
+
+    /**
+     * 获取用于检索实体的工具
+     */
+    fun getEntityFinder() = api.getEntityFinder()
+
+    /**
+     * 获取用于读取配置文件中的单位的序列化接口
+     */
+    fun getEntitySerializer() = api.getEntitySerializer()
+
+    /**
+     * 获取用于处理单位类型的接口
+     */
+    fun getEntityTypeRegistry() = api.getEntityTypeRegistry()
+
+    /**
+     * 获取用于处理单位元数据的接口
+     */
+    fun getEntityMetadataRegistry() = api.getEntityMetadataRegistry()
+
+    /**
+     * 获取用于处理单位控制器的接口
+     */
+    fun getEntityControllerRegistry() = api.getEntityControllerRegistry()
+
+    /**
+     * 获取 Hologram 控制接口
+     */
+    fun getHologramHandler() = api.getHologramHandler()
+
+    /**
+     * 获取 Kether 控制接口
+     */
+    fun getKetherHandler() = api.getKetherHandler()
+
+    /**
+     * 获取 Adyeshach 中的 NMS 接口
+     */
+    fun getMinecraftAPI() = api.getMinecraftAPI()
+
+    /**
+     * 获取 Adyeshach 中的网络接口
+     */
+    fun getNetworkAPI() = api.getNetworkAPI()
+
+    /**
+     * 获取 Adyeshach 中的语言文件接口
+     */
+    fun getLanguage() = api.getLanguage()
+
+    /**
+     * 获取事件总线
+     */
+    fun getEventBus() = api.getEventBus()
+}
