b91d3e3a562e3456f2ab3b84d17878acab8ed1ae
4o4E <869951226@qq.com>
2023-01-29 02:11:50 +0800

    添加orangeengine的hook

---
+++ eplugin-hook-orangeengine/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    `maven-publish`
+    `java-library`
+}
+
+group = Versions.group
+version = Versions.version
+
+repositories {
+    mavenLocal()
+    // spigot
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/groups/public/")
+    mavenCentral()
+}
+
+dependencies {
+    // spigot
+    compileOnly("org.spigotmc:spigot-api:${Versions.spigot}")
+    // oe
+    compileOnly(fileTree("libs"))
+    // eplugin
+    implementation(project(":eplugin-core"))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+afterEvaluate {
+    publishing.publications.create<MavenPublication>("java") {
+        from(components["kotlin"])
+        artifact(tasks.getByName("sourcesJar"))
+        artifact(tasks.getByName("javadocJar"))
+        artifactId = project.name
+        groupId = Versions.group
+        version = Versions.version
+    }
+}
---
+++ eplugin-hook-orangeengine/src/main/kotlin/OrangeEngineHook.kt
@@ -0,0 +1,18 @@
+package top.e404.eplugin.hook.oe
+
+import ltd.icecold.orangeengine.api.OrangeEngineAPI
+import ltd.icecold.orangeengine.api.data.model.ModelType
+import ltd.icecold.orangeengine.api.model.ModelManager
+import top.e404.eplugin.EPlugin
+import top.e404.eplugin.hook.EHook
+import java.util.*
+
+@Suppress("UNUSED")
+open class OrangeEngineHook(
+    override val plugin: EPlugin,
+) : EHook(plugin, "OrangeEngine") {
+    val modelManager: ModelManager
+        get() = OrangeEngineAPI.getModelManager()!!
+
+    fun setModel(id: UUID, model: String, type: ModelType = ModelType.BLOCKBENCH) = modelManager.addModelEntity(id, model, type)!!
+}
