7f21291c1ffb53bf5bbbcd7025d9c4b091e99481
4o4E <869951226@qq.com>
2023-01-29 02:12:23 +0800

    添加mmocore版本

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -9,6 +9,7 @@ object Versions {
 object Hooks {
     const val slimefun = "RC-30"
     const val mmoitems = "6.7.3"
+    const val mmocore = "1.9.3"
     const val mythicLib = "1.4"
     const val mythicmobs = "5.1.4"
     const val worldguard = "7.0.8-SNAPSHOT"
