e11180909066dfefc971a72a8750101683085eab
4o4E <869951226@qq.com>
2023-02-08 19:04:33 +0800

    更新依赖版本

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,10 @@
 object Versions {
-    const val kotlin = "1.7.10"
+    const val kotlin = "1.7.22"
     const val spigot = "1.13.2-R0.1-SNAPSHOT"
     const val group = "top.e404"
     const val version = "1.0.5"
-    const val kaml = "0.47.0"
+    const val kaml = "0.49.0"
+    const val serialization = "1.4.0"
 }
 
 object Hooks {
