25bfc139a24577cb18265993f842f856c724bd35
4o4E <869951226@qq.com>
2023-03-02 23:54:34 +0800

    添加Location和Vector加减乘除余的operator方法, 比较两个Location的block位置是否相等的Location.locationEquals(other: Location), 以及向上向下取整的方法

---
+++ eplugin-core/src/main/kotlin/util/location.kt
@@ -0,0 +1,104 @@
+package top.e404.eplugin.util
+
+import org.bukkit.Location
+import org.bukkit.util.Vector
+import kotlin.math.ceil
+import kotlin.math.floor
+
+operator fun Location.plus(other: Location) = apply {
+    x += other.x
+    y += other.y
+    z += other.z
+}
+
+operator fun Location.minus(other: Location) = apply {
+    x -= other.x
+    y -= other.y
+    z -= other.z
+}
+
+operator fun Location.plus(value: Double) = apply {
+    x += value
+    y += value
+    z += value
+}
+
+operator fun Location.minus(value: Double) = apply {
+    x -= value
+    y -= value
+    z -= value
+}
+
+operator fun Location.times(value: Double) = apply {
+    x *= value
+    y *= value
+    z *= value
+}
+
+operator fun Location.div(value: Double) = apply {
+    x /= value
+    y /= value
+    z /= value
+}
+
+operator fun Location.rem(value: Double) = apply {
+    x %= value
+    y %= value
+    z %= value
+}
+
+operator fun Vector.plus(other: Vector) = apply {
+    x += other.x
+    y += other.y
+    z += other.z
+}
+
+operator fun Vector.minus(other: Vector) = apply {
+    x -= other.x
+    y -= other.y
+    z -= other.z
+}
+
+operator fun Vector.plus(value: Double) = apply {
+    x += value
+    y += value
+    z += value
+}
+
+operator fun Vector.minus(value: Double) = apply {
+    x -= value
+    y -= value
+    z -= value
+}
+
+operator fun Vector.times(value: Double) = apply {
+    x *= value
+    y *= value
+    z *= value
+}
+
+operator fun Vector.div(value: Double) = apply {
+    x /= value
+    y /= value
+    z /= value
+}
+
+operator fun Vector.rem(value: Double) = apply {
+    x %= value
+    y %= value
+    z %= value
+}
+
+fun Location.locationEquals(other: Location) = blockX == other.blockX && blockY == other.blockY && blockZ == other.blockZ
+
+fun Location.ceil() = apply {
+    x = ceil(x)
+    y = ceil(y)
+    z = ceil(z)
+}
+
+fun Location.floor() = apply {
+    x = floor(x)
+    y = floor(y)
+    z = floor(z)
+}
