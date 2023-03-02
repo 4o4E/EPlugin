fb02099aeb9173e5027a8989ee179f38313ef169
4o4E <869951226@qq.com>
2023-03-02 23:55:49 +0800

    倒计时进度条CountdownBar

--- eplugin-core/src/main/kotlin/bossbar/CountdownBar.kt
+++ eplugin-core/src/main/kotlin/bossbar/CountdownBar.kt
@@ -8,32 +8,42 @@ import org.bukkit.entity.Player
 import org.bukkit.scheduler.BukkitTask
 import top.e404.eplugin.EPlugin
 
-class ProgressBar(
+/**
+ * 倒计时 BossBar
+ *
+ * @property plugin 注册的插件
+ * @property start 初始值
+ * @property display
+ */
+open class CountdownBar(
     val plugin: EPlugin,
     val start: Long,
-    val end: Long,
-    val step: Long,
-    val duration: Long,
-    val display: ProgressBar.() -> String,
+    val color: BarColor = BarColor.WHITE,
+    val style: BarStyle = BarStyle.SOLID,
+    val display: CountdownBar.() -> String,
 ) {
-    var now = start
+    var isStarted = false
+        private set
+    var tick = start
+        private set
     lateinit var bossBar: BossBar
         private set
     lateinit var task: BukkitTask
         private set
 
     fun start(vararg p: Player) {
-        bossBar = Bukkit.createBossBar(display(), BarColor.WHITE, BarStyle.SOLID)
+        bossBar = Bukkit.createBossBar(display(), color, style)
         p.forEach { bossBar.addPlayer(it) }
-        task = plugin.runTaskTimer(0, duration) {
-            now += step
-            if (now >= end) {
+        task = plugin.runTaskTimer(0, 20) {
+            if (tick <= 0) {
                 stop()
                 return@runTaskTimer
             }
             bossBar.setTitle(display())
-            bossBar.progress = (end - now).toDouble() / (end - start)
+            bossBar.progress = tick.toDouble() / start
+            tick--
         }
+        isStarted = true
     }
 
     fun stop() {
