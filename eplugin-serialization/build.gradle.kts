1bfab773bc0a3daa865aaf3826a3027a7e52ead6
4o4E <869951226@qq.com>
2023-03-07 02:07:37 +0800

    同步serialization-json的版本

--- eplugin-serialization/build.gradle.kts
+++ eplugin-serialization/build.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
     implementation(project(":eplugin-core"))
     // serialization
     api(kotlinx("serialization-core-jvm", Versions.serialization))
-    api(kotlinx("serialization-json", "1.3.3"))
+    api(kotlinx("serialization-json", Versions.serialization))
     // kaml
     api("com.charleskorn.kaml:kaml:${Versions.kaml}")
 }
