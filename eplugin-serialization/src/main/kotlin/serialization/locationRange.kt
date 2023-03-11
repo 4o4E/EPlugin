d123a1dc1856307b6a7cd7930cd5ed3d18074e51
4o4E <869951226@qq.com>
2023-03-11 21:34:00 +0800

    将LocationRange中的world改成字符串, 避免在世界未加载时加载Location

--- eplugin-serialization/src/main/kotlin/serialization/locationRange.kt
+++ eplugin-serialization/src/main/kotlin/serialization/locationRange.kt
@@ -4,13 +4,12 @@ package top.e404.eplugin.config.serialization
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.bukkit.Bukkit
 import org.bukkit.Location
-import org.bukkit.World
 
 @Serializable
 data class LocationRange(
-    @Serializable(WorldSerializer::class)
-    val world: World,
+    val world: String,
     @SerialName("x")
     @Serializable(DoubleRangeSerialization::class)
     val xRange: ClosedFloatingPointRange<Double>,
@@ -24,14 +23,13 @@ data class LocationRange(
     operator fun contains(l: Location) = contains(l.x, l.y, l.z)
     fun contains(x: Double, y: Double, z: Double) = x in xRange && y in yRange && z in zRange
     fun contains(x: Int, y: Int, z: Int) = contains(x.toDouble(), y.toDouble(), z.toDouble())
-    val center by lazy {
-        Location(
-            world,
+    val center
+        get() = Location(
+            Bukkit.getWorld(world),
             xRange.start + (xRange.endInclusive - xRange.start) / 2,
             yRange.start + (yRange.endInclusive - yRange.start) / 2,
             zRange.start + (zRange.endInclusive - zRange.start) / 2
         )
-    }
     val xLength get() = xRange.endInclusive - xRange.start
     val yLength get() = yRange.endInclusive - yRange.start
     val zLength get() = zRange.endInclusive - zRange.start
@@ -39,8 +37,7 @@ data class LocationRange(
 
 @Serializable
 data class IntLocationRange(
-    @Serializable(WorldSerializer::class)
-    val world: World,
+    val world: String,
     @SerialName("x")
     @Serializable(IntRangeSerialization::class)
     val xRange: IntRange,
@@ -54,20 +51,20 @@ data class IntLocationRange(
     operator fun contains(l: Location) = contains(l.x, l.y, l.z)
     fun contains(x: Int, y: Int, z: Int) = x in xRange && y in yRange && z in zRange
     fun contains(x: Double, y: Double, z: Double) = contains(x.toInt(), y.toInt(), z.toInt())
-    val center by lazy {
-        Location(
-            world,
+    val center
+        get() = Location(
+            Bukkit.getWorld(world),
             xRange.first + (xRange.last.toDouble() - xRange.first) / 2,
             yRange.first + (yRange.last.toDouble() - yRange.first) / 2,
             zRange.first + (zRange.last.toDouble() - zRange.first) / 2
         )
-    }
     val xLength get() = xRange.last - xRange.first
     val yLength get() = yRange.last - yRange.first
     val zLength get() = zRange.last - zRange.first
     override fun iterator() = iterator {
+        val bkw = Bukkit.getWorld(world)
         for (x in xRange) for (y in yRange) for (z in zRange) {
-            yield(Location(world, x.toDouble(), y.toDouble(), z.toDouble()))
+            yield(Location(bkw, x.toDouble(), y.toDouble(), z.toDouble()))
         }
     }
 }
