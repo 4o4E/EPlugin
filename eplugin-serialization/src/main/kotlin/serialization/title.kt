d8b08ccd184684ccab0a1a70f3388dacc8c2b109
4o4E <869951226@qq.com>
2023-03-14 21:42:22 +0800

    更新title的序列化

--- eplugin-serialization/src/main/kotlin/serialization/title.kt
+++ eplugin-serialization/src/main/kotlin/serialization/title.kt
@@ -1,5 +1,6 @@
 package top.e404.eplugin.config.serialization
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.bukkit.entity.Player
 import top.e404.eplugin.util.forEachOnline
@@ -8,9 +9,9 @@ import top.e404.eplugin.util.forEachOnline
 data class Title(
     val title: String = "",
     val subtitle: String = "",
-    val fadeIn: Int = 10,
+    @SerialName("fadein") val fadeIn: Int = 10,
     val stay: Int = 20,
-    val fadeOut: Int = 20,
+    @SerialName("fadeout") val fadeOut: Int = 20,
 ) {
     fun show(p: Player) = p.sendTitle(title, subtitle, fadeIn, stay, fadeOut)
     fun showAll() = forEachOnline(::show)
