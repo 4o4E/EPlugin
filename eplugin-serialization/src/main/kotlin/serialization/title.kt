49ff994c958a3db478be668c63a2b6bf5aaeea36
4o4E <869951226@qq.com>
2023-04-04 11:44:26 +0800

    添加 Title.display 和 Title.displayAll

--- eplugin-serialization/src/main/kotlin/serialization/title.kt
+++ eplugin-serialization/src/main/kotlin/serialization/title.kt
@@ -1,9 +1,12 @@
+@file:Suppress("UNUSED")
+
 package top.e404.eplugin.config.serialization
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import org.bukkit.entity.Player
 import top.e404.eplugin.EPlugin.Companion.color
+import top.e404.eplugin.EPlugin.Companion.placeholder
 import top.e404.eplugin.util.forEachOnline
 
 @Serializable
@@ -16,4 +19,22 @@ data class Title(
 ) {
     fun show(p: Player) = p.sendTitle(title.color(), subtitle.color(), fadeIn, stay, fadeOut)
     fun showAll() = forEachOnline(::show)
+
+    fun display(p: Player, vararg placeholder: Pair<String, *>) = p.sendTitle(
+        title.placeholder(*placeholder),
+        subtitle.placeholder(*placeholder),
+        fadeIn,
+        stay,
+        fadeOut
+    )
+
+    fun displayAll(vararg placeholder: Pair<String, *>) = forEachOnline {
+        it.sendTitle(
+            title.placeholder(*placeholder),
+            subtitle.placeholder(*placeholder),
+            fadeIn,
+            stay,
+            fadeOut
+        )
+    }
 }
