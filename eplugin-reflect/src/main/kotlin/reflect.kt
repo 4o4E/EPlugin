d68791c80161768969c05221d4aeace8ef19cdd9
4o4E <869951226@qq.com>
2023-04-29 15:07:39 +0800

    移除无用的static字段, 添加反射的构造器支持

--- eplugin-reflect/src/main/kotlin/reflect.kt
+++ eplugin-reflect/src/main/kotlin/reflect.kt
@@ -31,72 +31,183 @@ data class MethodKey(
     }
 }
 
-class InvalidFieldException(
-    clazz: Class<*>,
-    static: Boolean,
-    field: String,
-    e: NoSuchFieldException,
-) : Exception(
-    """invalid${if (static) " static" else ""} field($field) in class(${clazz.name})
-        |valid${if (static) " static" else ""} field: ${(if (static) clazz.declaredFields else clazz.fields).joinToString(", ") { it.name }}
-    """.trimMargin(),
-    e
-)
-
-class InvalidMethodException(
-    clazz: Class<*>,
-    static: Boolean,
-    method: String,
-    paramTypes: Array<Class<*>>,
-    e: NoSuchMethodException,
+class InvalidConstructorException(
+    cls: Class<*>,
+    parameters: Array<Class<*>>,
 ) : Exception() {
-    private val params = paramTypes.joinToString(", ") { it.name }
-    private val validMethods = (if (static) clazz.methods else clazz.declaredMethods).joinToString("") { m ->
-        "\n  ${m.returnType.name} ${m.name}(${m.parameterTypes.joinToString(", ") { it.name }}"
+    override val message = run {
+        val params = parameters.joinToString(", ") { it.name }
+        val valid = cls.declaredConstructors.joinToString("\n  ") { "${it.name}()" }
+        """invalid constructor($params) in class(${cls.name})
+            |valid constructor: $valid
+        """.trimMargin()
     }
-    override val message = """invalid${if (static) " static" else ""} method($method)
-        |  params[$params] in class(${clazz.name})
-        |valid${if (static) " static" else ""} method: $validMethods""".trimMargin()
+}
+
+class InvalidFieldException(
+    cls: Class<*>,
+    field: String,
+    e: Exception,
+) : Exception() {
+    override val message = run {
+        val fields = cls.declaredFields.joinToString(", ") { it.name }
+        """invalid field($field) in class(${cls.name})
+            |valid field: [$fields]
+        """.trimMargin()
+    }
     override val cause = e
 }
 
-class ReflectData<T>(val clazz: Class<T>) {
-    val name: String = clazz.name
+class InvalidMethodException(
+    cls: Class<*>,
+    method: String,
+    paramTypes: Array<Class<*>>,
+    e: Exception,
+) : Exception() {
+    override val message = run {
+        val params = paramTypes.joinToString(", ") { it.name }
+        val validMethods = cls.declaredMethods.joinToString("") { m ->
+            "\n  ${m.returnType.name} ${m.name}(${m.parameterTypes.joinToString(", ") { it.name }}"
+        }
+        """invalid method($method)
+            |  params[$params] in class(${cls.name})
+            |valid method: $validMethods
+        """.trimMargin()
+    }
+    override val cause = e
+}
+
+class ReflectData<T>(val cls: Class<T>) {
+    val name: String = cls.name
     val fields = mutableMapOf<String, Field>()
     val methods = mutableMapOf<MethodKey, Method>()
-    val annotation get() = clazz.annotations
+    val annotation get() = cls.annotations
+
+    fun newInstance(): T = try {
+        cls.getDeclaredConstructor()
+    } catch (e: Exception) {
+        throw InvalidConstructorException(cls, emptyArray())
+    }.newInstance()
+
+    inline fun <reified P0: Any> newInstance(arg0: P0): T {
+        val arg0cls = arg0.javaClass
+        val constructor = cls.constructors.firstOrNull {
+            it.parameterCount == 1 && it.parameters[0].type == arg0cls
+        } ?: throw InvalidConstructorException(cls, arrayOf(arg0cls))
+        return constructor.newInstance(arg0) as T
+    }
+
+    inline fun <reified P0: Any, reified P1: Any> newInstance(arg0: P0, arg1: P1): T {
+        val arg0cls = arg0.javaClass
+        val arg1cls = arg1.javaClass
+        val constructor = cls.constructors.firstOrNull {
+            it.parameterCount == 2
+                    && it.parameters[0].type == arg0cls
+                    && it.parameters[1].type == arg1cls
+        } ?: throw InvalidConstructorException(cls, arrayOf(arg0cls, arg1cls))
+        return constructor.newInstance(arg0, arg1) as T
+    }
+
+    inline fun <reified P0: Any, reified P1: Any, reified P2: Any> newInstance(arg0: P0, arg1: P1, arg2: P2): T {
+        val arg0cls = arg0.javaClass
+        val arg1cls = arg1.javaClass
+        val arg2cls = arg2.javaClass
+        val constructor = cls.constructors.firstOrNull {
+            it.parameterCount == 3
+                    && it.parameters[0].type == arg0cls
+                    && it.parameters[1].type == arg1cls
+                    && it.parameters[2].type == arg2cls
+        } ?: throw InvalidConstructorException(cls, arrayOf(arg0cls, arg1cls, arg2cls))
+        return constructor.newInstance(arg0, arg1, arg2) as T
+    }
+
+    inline fun <reified P0: Any, reified P1: Any, reified P2: Any, reified P3: Any> newInstance(
+        arg0: P0,
+        arg1: P1,
+        arg2: P2,
+        arg3: P3,
+    ): T {
+        val arg0cls = arg0.javaClass
+        val arg1cls = arg1.javaClass
+        val arg2cls = arg2.javaClass
+        val arg3cls = arg3.javaClass
+        val constructor = cls.constructors.firstOrNull {
+            it.parameterCount == 4
+                    && it.parameters[0].type == arg0cls
+                    && it.parameters[1].type == arg1cls
+                    && it.parameters[2].type == arg2cls
+                    && it.parameters[3].type == arg3cls
+        } ?: throw InvalidConstructorException(cls, arrayOf(arg0cls, arg1cls, arg2cls, arg3cls))
+        return constructor.newInstance(arg0, arg1, arg2, arg3) as T
+    }
+
+    inline fun <reified P0: Any, reified P1: Any, reified P2: Any, reified P3: Any, reified P4: Any> newInstance(
+        arg0: P0,
+        arg1: P1,
+        arg2: P2,
+        arg3: P3,
+        arg4: P4,
+    ): T {
+        val arg0cls = arg0.javaClass
+        val arg1cls = arg1.javaClass
+        val arg2cls = arg2.javaClass
+        val arg3cls = arg3.javaClass
+        val arg4cls = arg4.javaClass
+        val constructor = cls.constructors.firstOrNull {
+            it.parameterCount == 5
+                    && it.parameters[0].type == arg0cls
+                    && it.parameters[1].type == arg1cls
+                    && it.parameters[2].type == arg2cls
+                    && it.parameters[3].type == arg3cls
+                    && it.parameters[4].type == arg4cls
+        } ?: throw InvalidConstructorException(cls, arrayOf(arg0cls, arg1cls, arg2cls, arg3cls, arg4cls))
+        return constructor.newInstance(arg0, arg1, arg2, arg3, arg4) as T
+    }
 
     fun <R> getField(
         obj: T,
         name: String,
-        static: Boolean = false,
     ): R {
         val exists = fields[name]
         if (exists != null) return exists.get(obj) as R
         val field = try {
-            if (static) clazz.getField(name)
-            else clazz.getDeclaredField(name)
+            cls.getDeclaredField(name)
         } catch (e: NoSuchFieldException) {
-            throw InvalidFieldException(clazz, static, name, e)
+            throw InvalidFieldException(cls, name, e)
         }
         field.isAccessible = true
         fields[name] = field
         return field.get(obj) as R
     }
 
+    fun <R> setField(
+        obj: T,
+        name: String,
+        value: R,
+    ) {
+        val exists = fields[name]
+        if (exists != null) return exists.set(obj, value)
+        val field = try {
+            cls.getDeclaredField(name)
+        } catch (e: NoSuchFieldException) {
+            throw InvalidFieldException(cls, name, e)
+        }
+        field.isAccessible = true
+        fields[name] = field
+        return field.set(obj, value)
+    }
+
     fun getMethod(
         name: String,
-        static: Boolean = false,
         paramTypes: Array<Class<*>>,
     ): Method {
         val key = MethodKey(name, paramTypes)
         val exists = methods[key]
         if (exists != null) return exists
         val method = try {
-            if (static) clazz.getMethod(name, *paramTypes)
-            else clazz.getDeclaredMethod(name)
+            cls.getDeclaredMethod(name)
         } catch (e: NoSuchMethodException) {
-            throw InvalidMethodException(clazz, static, name, paramTypes, e)
+            throw InvalidMethodException(cls, name, paramTypes, e)
         }
         method.isAccessible = true
         methods[key] = method
@@ -106,10 +217,9 @@ class ReflectData<T>(val clazz: Class<T>) {
     fun <R> invokeMethod(
         obj: T,
         name: String,
-        static: Boolean = false,
         paramTypes: Array<Class<*>>,
         params: Array<*>,
-    ) = getMethod(name, static, paramTypes).invoke(obj, *params) as R
+    ) = getMethod(name, paramTypes).invoke(obj, *params) as R
 }
 
 inline val <reified T> Class<T>.reflectData get() = reflectDataMap.getOrPut(this) { ReflectData(this) } as ReflectData<T>
@@ -117,19 +227,21 @@ inline val <reified T : Any> T.reflectData get() = T::class.java.reflectData
 
 inline fun <reified T : Any, reified R> T.getPrivateField(
     name: String,
-    static: Boolean = false,
-) = reflectData.getField(this, name, static) as R
+) = reflectData.getField(this, name) as R
+
+inline fun <reified T : Any, reified R> T.setPrivateField(
+    name: String,
+    value: R,
+) = reflectData.setField(this, name, value)
 
 inline fun <reified T : Any, reified R> T.getPrivateMethod(
     name: String,
-    static: Boolean = false,
     paramTypes: Array<Class<*>>,
-): Method = reflectData.getMethod(name, static, paramTypes)
+): Method = reflectData.getMethod(name, paramTypes)
 
 inline fun <reified T : Any, reified R> T.invokePrivateMethod(
     obj: T,
     name: String,
-    static: Boolean = false,
     paramTypes: Array<Class<*>>,
     params: Array<*>,
-) = reflectData.invokeMethod(obj, name, static, paramTypes, params) as R
+) = reflectData.invokeMethod(obj, name, paramTypes, params) as R
