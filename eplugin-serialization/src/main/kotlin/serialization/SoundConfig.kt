1fc287a17f52ca62382791f71aeb53d2d4bf0bb9
4o4E <869951226@qq.com>
2023-05-03 16:21:10 +0800

    支持BkSerializable

--- eplugin-serialization/src/main/kotlin/serialization/SoundConfig.kt
+++ eplugin-serialization/src/main/kotlin/serialization/SoundConfig.kt
@@ -8,15 +8,19 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import org.bukkit.Location
 import org.bukkit.Sound
+import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.entity.Player
 import top.e404.eplugin.EPlugin.Companion.formatAsConst
+import top.e404.eplugin.config.BkSerializable
+import top.e404.eplugin.config.buildYamlConfiguration
+import top.e404.eplugin.config.getFloat
 
 @Serializable
 data class SoundConfig(
     var type: String,
     var volume: Float,
     var pitch: Float,
-) {
+) : BkSerializable {
     private inline val format get() = if (':' in type) type else "minecraft:$type"
 
     fun playAt(location: Location) = location.world?.playSound(location, format, volume, pitch)
@@ -24,6 +28,25 @@ data class SoundConfig(
     fun playTo(p: Player) = p.playSound(p.location, format, volume, pitch)
 
     fun playTo(players: Collection<Player>) = players.forEach(::playTo)
+
+    companion object {
+        init {
+            BkSerializable.regBkSerializable<SoundConfig>()
+        }
+
+        @JvmStatic
+        fun deserialize(data: ConfigurationSection) = SoundConfig(
+            data.getString("type")!!,
+            data.getFloat("volume"),
+            data.getFloat("pitch"),
+        )
+    }
+
+    override fun serialize() = buildYamlConfiguration {
+        set("type", type)
+        set("volume", volume)
+        set("pitch", pitch)
+    }
 }
 
 object SoundSerializer : KSerializer<Sound> {
--- eplugin-serialization/src/main/kotlin/serialization/title.kt
+++ eplugin-serialization/src/main/kotlin/serialization/title.kt
@@ -4,9 +4,12 @@ package top.e404.eplugin.config.serialization
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.entity.Player
 import top.e404.eplugin.EPlugin.Companion.color
 import top.e404.eplugin.EPlugin.Companion.placeholder
+import top.e404.eplugin.config.BkSerializable
+import top.e404.eplugin.config.buildYamlConfiguration
 import top.e404.eplugin.util.forEachOnline
 
 @Serializable
@@ -16,7 +19,7 @@ data class Title(
     @SerialName("fadein") val fadeIn: Int = 10,
     val stay: Int = 20,
     @SerialName("fadeout") val fadeOut: Int = 20,
-) {
+) : BkSerializable {
     fun show(p: Player) = p.sendTitle(title.color(), subtitle.color(), fadeIn, stay, fadeOut)
     fun showAll() = forEachOnline(::show)
 
@@ -36,5 +39,28 @@ data class Title(
             stay,
             fadeOut
         )
+    }
+
+    companion object {
+        init {
+            BkSerializable.regBkSerializable<Title>()
+        }
+
+        @JvmStatic
+        fun deserialize(data: ConfigurationSection) = Title(
+            data.getString("title") ?: "",
+            data.getString("subtitle") ?: "",
+            data.getInt("fadeIn"),
+            data.getInt("stay"),
+            data.getInt("fadeOut"),
+        )
+    }
+
+    override fun serialize() = buildYamlConfiguration {
+        set("title", title)
+        set("subtitle", subtitle)
+        set("fadeIn", fadeIn)
+        set("stay", stay)
+        set("fadeOut", fadeOut)
     }
 }
