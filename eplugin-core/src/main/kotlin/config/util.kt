b3e63f60760f15d32663b1fadc1e6ea5bcc2a38a
4o4E <869951226@qq.com>
2023-05-03 16:19:26 +0800

    添加buildYamlConfiguration(block: YamlConfiguration.() -> Unit), ConfigurationSection.getFloat(path: String)

--- eplugin-core/src/main/kotlin/config/util.kt
+++ eplugin-core/src/main/kotlin/config/util.kt
@@ -1,3 +1,14 @@
 package top.e404.eplugin.config
 
+import org.bukkit.configuration.ConfigurationSection
+import org.bukkit.configuration.file.YamlConfiguration
+
 fun List<Regex>.matches(string: String) = any { it.matches(string) }
+
+inline fun buildYamlConfiguration(block: YamlConfiguration.() -> Unit) = YamlConfiguration().apply(block)
+
+fun ConfigurationSection.getFloat(path: String) = when (val v = get(path)) {
+    is Number -> v.toFloat()
+    is String -> v.toFloat()
+    else -> 0F
+}
