70d17660c9d49a19961a18f8259c2ad6d1b464e7
4o4E <869951226@qq.com>
2023-05-05 00:38:44 +0800

    将inv提取至接口

--- eplugin-menu/src/main/kotlin/InventoryClickHandler.kt
+++ eplugin-menu/src/main/kotlin/InventoryClickHandler.kt
@@ -1,12 +1,18 @@
 package top.e404.eplugin.menu
 
 import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.inventory.Inventory
 import org.bukkit.inventory.ItemStack
 
 /**
  * 代表一个处理菜单点击事件的对象
  */
 interface InventoryClickHandler {
+    /**
+     * 对应的[Inventory]
+     */
+    val inv: Inventory
+
     /**
      * 玩家空手点击空格子
      *
@@ -60,6 +66,24 @@ interface InventoryClickHandler {
         clicked: ItemStack,
         cursor: ItemStack,
         slot: Int,
+        event: InventoryClickEvent
+    ): Boolean?
+
+    /**
+     * 玩家通过1-9的快捷键交换菜单中物品
+     *
+     * @param target 光标指着的物品
+     * @param hotbarItem 通过快捷键确定的槽位中的物品
+     * @param slot 光标指着的物品的下标
+     * @param hotbar 快捷键下标
+     * @param event 时间
+     * @return 若返回true则取消事件, 返回null代表未处理
+     */
+    fun onHotbarAction(
+        target: ItemStack?,
+        hotbarItem: ItemStack?,
+        slot: Int,
+        hotbar: Int,
         event: InventoryClickEvent
     ): Boolean?
 }
