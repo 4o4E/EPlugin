99d10052d4080271fe801d43706fe247d6db811c
4o4E <869951226@qq.com>
2023-05-09 19:54:40 +0800

    添加新的物品序列化模型

---
+++ eplugin-serialization/src/main/kotlin/serialization/Item.kt
@@ -0,0 +1,53 @@
+@file:Suppress("UNUSED")
+
+package top.e404.eplugin.config.serialization
+
+import kotlinx.serialization.Serializable
+import org.bukkit.Material
+import org.bukkit.attribute.Attribute
+import org.bukkit.attribute.AttributeModifier
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.inventory.ItemFlag
+import top.e404.eplugin.EPlugin.Companion.formatAsConst
+import top.e404.eplugin.util.buildItemStack
+import java.util.*
+
+@Serializable
+data class Item(
+    @Serializable(MaterialSerializer::class) val material: Material,
+    val amount: Int = 1,
+    val name: String? = null,
+    val lore: List<String> = emptyList(),
+    val enchant: Map<@Serializable(EnchantmentSerialization::class) Enchantment, Int> = emptyMap(),
+    val flag: Set<ItemFlag> = emptySet(),
+    val attribute: Map<String, Double> = emptyMap(),
+    val unbreakable: Boolean = false
+) {
+    private val origin by lazy {
+        buildItemStack(material, amount, name, lore) {
+            enchant.forEach { (enchant, level) -> addEnchant(enchant, level, true) }
+            flag.forEach { addItemFlags(it) }
+            attribute.forEach { (attr, amount) ->
+                addAttributeModifier(
+                    try {
+                        Attribute.valueOf(attr.formatAsConst())
+                    } catch (e: IllegalArgumentException) {
+                        throw Exception("can't find attribute with name of $attr")
+                    },
+                    AttributeModifier(UUID.randomUUID(), "", amount, AttributeModifier.Operation.ADD_NUMBER)
+                )
+            }
+            if (unbreakable) isUnbreakable = true
+        }
+    }
+    val item get() = origin.clone()
+}
+
+@Serializable
+data class AttributeModifierModel(
+    val name: String = "",
+    val amount: Double,
+    val operation: String
+) {
+    fun toAttributeModifier() = AttributeModifier(UUID.randomUUID(), name, amount, AttributeModifier.Operation.valueOf(operation.formatAsConst()))
+}
