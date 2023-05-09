f86e715519b331861ee1803181b4c4c51b702e7b
4o4E <869951226@qq.com>
2023-05-09 19:52:35 +0800

    添加castSkill工具方法

--- eplugin-hook-mythicmobs/src/main/kotlin/MythicMobsHook.kt
+++ eplugin-hook-mythicmobs/src/main/kotlin/MythicMobsHook.kt
@@ -1,9 +1,14 @@
 package top.e404.eplugin.hook.mmoitems
 
 import io.lumine.mythic.api.items.ItemManager
+import io.lumine.mythic.api.mobs.GenericCaster
 import io.lumine.mythic.api.volatilecode.handlers.VolatileItemHandler
 import io.lumine.mythic.bukkit.BukkitAPIHelper
+import io.lumine.mythic.bukkit.BukkitAdapter
 import io.lumine.mythic.bukkit.MythicBukkit
+import io.lumine.mythic.core.skills.SkillMetadataImpl
+import io.lumine.mythic.core.skills.SkillTriggers
+import io.lumine.mythic.core.skills.variables.Variable
 import org.bukkit.Location
 import org.bukkit.entity.Entity
 import org.bukkit.inventory.Inventory
@@ -39,6 +44,30 @@ open class MythicMobsHook(
     fun getMythicMobByEntity(entity: Entity) = apiHelper.getMythicMobInstance(entity)
 
     fun spawnMythicMob(type: String, location: Location) = apiHelper.spawnMythicMob(type, location)
+
+    fun castSkill(
+        entity: Entity,
+        skillName: String,
+        vararg variables: Pair<String, Variable>
+    ) {
+        val skill = MythicBukkit.inst().skillManager.getSkill(skillName).get()
+        val adaptedEntity = BukkitAdapter.adapt(entity)
+        val adaptedLocation = BukkitAdapter.adapt(entity.location)
+        val caster =
+            if (MythicBukkit.inst().mobManager.isActiveMob(entity.uniqueId)) MythicBukkit.inst().mobManager.getMythicMobInstance(entity)
+            else GenericCaster(adaptedEntity)
+        val data = SkillMetadataImpl(
+            SkillTriggers.API,
+            caster,
+            adaptedEntity,
+            adaptedLocation,
+            listOf(adaptedEntity),
+            listOf(adaptedLocation),
+            1F
+        )
+        variables.forEach { (key, variable) -> data.variables.put(key, variable) }
+        if (skill.isUsable(data, SkillTriggers.API)) skill.execute(data)
+    }
 
     fun count(
         inv: Inventory,
