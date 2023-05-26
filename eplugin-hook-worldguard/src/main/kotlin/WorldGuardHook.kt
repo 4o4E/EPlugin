5ca20a2d676869a5c2844b2999bf2bf6f616a5c4
4o4E <869951226@qq.com>
2023-05-26 23:53:57 +0800

    为ProtectedRegion添加forEach和forEachXZ方法用于遍历区域

--- eplugin-hook-worldguard/src/main/kotlin/WorldGuardHook.kt
+++ eplugin-hook-worldguard/src/main/kotlin/WorldGuardHook.kt
@@ -1,14 +1,16 @@
+@file:Suppress("UNUSED")
+
 package top.e404.eplugin.hook.worldguard
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter
 import com.sk89q.worldguard.WorldGuard
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin
+import com.sk89q.worldguard.protection.regions.ProtectedRegion
 import org.bukkit.Location
 import org.bukkit.World
 import top.e404.eplugin.EPlugin
 import top.e404.eplugin.hook.EHook
 
-@Suppress("UNUSED")
 open class WorldGuardHook(
     override val plugin: EPlugin,
 ) : EHook(plugin, "WorldGuard") {
@@ -27,4 +29,32 @@ open class WorldGuardHook(
     fun getRegion(world: World, name: String) =
         container.get(BukkitAdapter.adapt(world))
             ?.getRegion(name)
+}
+
+/**
+ * 遍历保护区域的xz坐标, 忽略y轴
+ */
+inline fun ProtectedRegion.forEachXZ(block: (x: Int, z: Int) -> Unit) {
+    val min = minimumPoint
+    val max = maximumPoint
+    for (x in min.blockX..max.blockX) for (z in min.blockZ..max.blockZ) {
+        block(x, z)
+    }
+}
+
+/**
+ * 遍历保护区域的坐标
+ *
+ * @param inner 若设置为true则跳过不包括在region中的方块
+ * @param block 操作
+ */
+inline fun ProtectedRegion.forEach(inner: Boolean = false, block: (x: Int, y: Int, z: Int) -> Unit) {
+    val min = minimumPoint
+    val max = maximumPoint
+    for (x in min.blockX..max.blockX) for (y in min.blockY..max.blockY) {
+        for (z in min.blockZ..max.blockZ) {
+            if (inner && !contains(x, y, z)) continue
+            block(x, y, z)
+        }
+    }
 }
