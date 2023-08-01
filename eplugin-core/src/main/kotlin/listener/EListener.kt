961968d95cf6ee8a19f10e0221d90ea348f22dd2
4o4E <869951226@qq.com>
2023-08-02 00:53:22 +0800

    添加自带的unregister方法, 使用反射来移除已注册的监听器

--- eplugin-core/src/main/kotlin/listener/EListener.kt
+++ eplugin-core/src/main/kotlin/listener/EListener.kt
@@ -1,9 +1,23 @@
 package top.e404.eplugin.listener
 
 import org.bukkit.Bukkit
+import org.bukkit.event.EventHandler
+import org.bukkit.event.HandlerList
 import org.bukkit.event.Listener
 import top.e404.eplugin.EPlugin
 
 abstract class EListener(open val plugin: EPlugin) : Listener {
     open fun register() = Bukkit.getPluginManager().registerEvents(this, plugin)
+
+    open fun unregister() {
+        this::class.java.declaredMethods.filter {
+            it.getDeclaredAnnotationsByType(EventHandler::class.java).isNotEmpty()
+        }.forEach { method ->
+            val cls = method.parameters[0].type
+            val handlerList = cls.getDeclaredField("handlers").also {
+                it.isAccessible = true
+            }.get(cls) as HandlerList
+            handlerList.unregister(this)
+        }
+    }
 }
