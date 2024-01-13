36a88a0b4f96546ed9ea57c3ea4d274e7cfc07d7
4o4E <869951226@qq.com>
2024-01-13 14:32:52 +0800

    fix: jitpack 域名补全www.

--- buildSrc/src/main/kotlin/repo.kt
+++ buildSrc/src/main/kotlin/repo.kt
@@ -4,6 +4,6 @@ import org.gradle.kotlin.dsl.maven
 fun RepositoryHandler.lumine() = maven("https://mvn.lumine.io/repository/maven-public/")
 fun RepositoryHandler.phoenix() = maven("https://nexus.phoenixdevt.fr/repository/maven-public/")
 fun RepositoryHandler.engineHub() = maven("https://maven.enginehub.org/repo/")
-fun RepositoryHandler.jitpack() = maven("https://jitpack.io")
+fun RepositoryHandler.jitpack() = maven("https://www.jitpack.io")
 fun RepositoryHandler.taboo() = maven("https://repo.tabooproject.org/repository/releases/")
 fun RepositoryHandler.codemc() = maven("https://repo.codemc.org/repository/maven-public/")
