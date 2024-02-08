b4b6f2c1ea99a28ad4364a7da226b211c0ef4074
4o4E <869951226@qq.com>
2024-02-08 16:37:02 +0800

    fix: 修复ConfigurationSerializable递归反序列化时ImmutableMap不可修改的问题

--- eplugin-core/src/main/kotlin/util/serialization.kt
+++ eplugin-core/src/main/kotlin/util/serialization.kt
@@ -3,7 +3,7 @@ package top.e404.eplugin.util
 import org.bukkit.configuration.serialization.ConfigurationSerializable
 
 fun ConfigurationSerializable.serializeRecursion(): MutableMap<String, Any> {
-    val map = this.serialize()
+    val map = LinkedHashMap(this.serialize())
     return map.onEach { (k, v) ->
         if (v is ConfigurationSerializable) map[k] = v.serializeRecursion()
     }
