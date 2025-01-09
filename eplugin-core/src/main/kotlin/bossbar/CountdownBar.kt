825d37dd66c777fa4416d11872fcc27ed0c03fad
4o4E <869951226@qq.com>
2025-01-09 21:42:28 +0800

    feat: CountdownManager

--- eplugin-core/src/main/kotlin/bossbar/CountdownBar.kt
+++ eplugin-core/src/main/kotlin/bossbar/CountdownBar.kt
@@ -20,57 +20,43 @@ open class CountdownBar(
     var start: Long,
     val color: BarColor = BarColor.WHITE,
     val style: BarStyle = BarStyle.SOLID,
-    val display: CountdownBar.() -> String,
+    var display: CountdownBar.() -> String,
 ) {
     var isStarted = false
         private set
     var tick = start
-        internal set
     lateinit var bossBar: BossBar
         private set
-    private lateinit var task: BukkitTask
+    private var task: BukkitTask? = null
 
-    var onEnd: (() -> Unit)? = null
+    var onEnd: (() -> Unit) = {
+        bossBar.removeAll()
+    }
 
-    fun start(vararg p: Player) {
+    var onTick: (() -> Unit) = {
+        bossBar.setTitle(display())
+        bossBar.progress = tick.toDouble() / start
+        tick--
+    }
+
+    fun start(vararg p: Player, enableTask: Boolean) {
         bossBar = Bukkit.createBossBar(display(), color, style)
         p.forEach { bossBar.addPlayer(it) }
-        task = plugin.runTaskTimer(0, 1) {
-            if (tick <= 0) {
-                it.cancel()
-                bossBar.removeAll()
-                onEnd?.invoke()
-                return@runTaskTimer
+        if (enableTask) {
+            task = plugin.runTaskTimer(0, 1) {
+                if (tick <= 0) {
+                    it.cancel()
+                    onEnd.invoke()
+                    return@runTaskTimer
+                }
+                onTick()
             }
-            bossBar.setTitle(display())
-            bossBar.progress = tick.toDouble() / start
-            tick--
         }
         isStarted = true
     }
 
     fun stop() {
-        task.cancel()
+        task?.cancel()
         bossBar.removeAll()
-    }
-}
-
-abstract class CountdownManager {
-    val players = mutableMapOf<Player, CountdownBar>()
-
-    abstract fun create(player: Player, cooldown: Long? = null): CountdownBar
-
-    fun add(player: Player, cooldown: Long? = null) {
-        players[player] = create(player, cooldown).apply {
-            onEnd = {
-                players.remove(player)
-            }
-            start(player)
-        }
-    }
-
-    fun shutdown() {
-        players.forEach { (_, bar) -> bar.stop() }
-        players.clear()
     }
 }
---
+++ eplugin-game/src/main/kotlin/util/CountdownManager.kt
@@ -0,0 +1,86 @@
+package top.e404.eplugin.game.util
+
+import org.bukkit.boss.BarColor
+import org.bukkit.boss.BarStyle
+import org.bukkit.entity.Player
+import top.e404.eplugin.EPlugin
+import top.e404.eplugin.EPlugin.Companion.placeholder
+import top.e404.eplugin.bossbar.CountdownBar
+import top.e404.eplugin.config.serialization.CountdownConfig
+
+/**
+ * 倒计时管理器
+ */
+@Suppress("UNUSED")
+abstract class CountdownManager(
+    val plugin: EPlugin,
+    val countdownConfig: CountdownConfig,
+    val enableInternalTask: Boolean
+) {
+    val players = mutableMapOf<Player, CountdownBar>()
+
+    open fun create(player: Player, cooldown: Long?): CountdownBar {
+        val start = cooldown ?: (countdownConfig.duration * 20L)
+        val bossbarConfig = countdownConfig.currentBar(start / 20)
+        return CountdownBar(
+            plugin, start,
+            bossbarConfig?.color ?: BarColor.WHITE,
+            bossbarConfig?.style ?: BarStyle.SOLID
+        ) {
+            bossbarConfig?.template?.placeholder(*getPlaceholder()) ?: ""
+        }
+    }
+
+    open fun CountdownBar.getPlaceholder(): Array<Pair<String, *>> = arrayOf(
+        "duration" to countdownConfig.duration,
+        "tick" to tick,
+        "second" to tick / 20
+    )
+
+    fun add(player: Player, cooldown: Long? = null): CountdownBar {
+        create(player, cooldown).apply {
+            val existsOnEnd = onEnd
+            onEnd = {
+                existsOnEnd()
+                players.remove(player)
+            }
+            onTick = {
+                try {
+                    if (tick % 20 == 0L) {
+                        val sec = tick / 20
+                        countdownConfig.currentBar(sec)?.let { config ->
+                            bossBar.color = config.color
+                            bossBar.style = config.style
+                            bossBar.setTitle(config.template.placeholder(*getPlaceholder()))
+                        } ?: bossBar.setTitle("")
+                        countdownConfig.currentMessage(sec)?.sendTo(player, *getPlaceholder())
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+                bossBar.progress = tick.toDouble() / start
+                tick--
+            }
+            start(player, enableTask = enableInternalTask)
+        }.let {
+            players[player] = it
+            return it
+        }
+    }
+
+    fun onTick() {
+        players.values.forEach { it.onTick() }
+    }
+
+    /**
+     * 从管理器中移除玩家并取消其显示
+     */
+    fun remove(player: Player) {
+        players.remove(player)?.stop()
+    }
+
+    fun shutdown() {
+        players.forEach { (_, bar) -> bar.stop() }
+        players.clear()
+    }
+}
