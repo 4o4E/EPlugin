6f4321d5b9a188eac6fcd90b029206fab6e8b03e
4o4E <869951226@qq.com>
2025-01-17 00:20:14 +0800

    feat: BlockParticle.spawnOnBlock

--- eplugin-serialization-particle/src/main/kotlin/BlockParticle.kt
+++ eplugin-serialization-particle/src/main/kotlin/BlockParticle.kt
@@ -5,7 +5,9 @@ import kotlinx.serialization.Serializable
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.Particle
+import org.bukkit.block.Block
 import top.e404.eplugin.EPlugin.Companion.formatAsConst
+import kotlin.random.Random
 
 @Serializable
 @SerialName("BlockData")
@@ -17,4 +19,18 @@ data class BlockParticle(
     val data: String,
 ) : ParticleConfig {
     override fun generator() = Bukkit.createBlockData(Material.valueOf(data.formatAsConst()))
+
+    fun spawnOnBlock(
+        block: Block,
+        offsetX: Double = 0.0,
+        offsetY: Double = 0.0,
+        offsetZ: Double = 0.0,
+    ) {
+        chance?.let { if (Random.nextDouble() > it) return }
+        block.world.spawnParticle(
+            particle, block.location, count,
+            offsetX, offsetY, offsetZ,
+            extra, Bukkit.createBlockData(block.type), false
+        )
+    }
 }
