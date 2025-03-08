0b0f0bf5a411377e7e7390c1af59fbf480596d50
4o4E <869951226@qq.com>
2025-03-08 17:27:28 +0800

    feat: SoundSerializer

--- eplugin-core/src/main/kotlin/util/config.kt
+++ eplugin-core/src/main/kotlin/util/config.kt
@@ -2,7 +2,12 @@
 
 package top.e404.eplugin.util
 
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import org.bukkit.Location
 import org.bukkit.Sound
 import org.bukkit.configuration.ConfigurationSection
@@ -20,11 +25,26 @@ fun ConfigurationSection.getSoundConfig(path: String) =
         val sound = Sound.valueOf(t.formatAsConst())
         val volume = cfg.getDouble("volume", 1.0).toFloat()
         val pitch = cfg.getDouble("pitch", 1.0).toFloat()
-        SoundConfig(sound, volume, pitch)
+        LegacySoundConfig(sound, volume, pitch)
     }
 
+object SoundSerializer : KSerializer<Sound> {
+    override val descriptor = PrimitiveSerialDescriptor(SoundSerializer::class.qualifiedName!!, PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): Sound {
+        val const = decoder.decodeString().formatAsConst()
+        return Sound.valueOf(const)
+    }
+
+    override fun serialize(encoder: Encoder, value: Sound) {
+        encoder.encodeString(value.name)
+    }
+
+}
+
 @Serializable
-data class SoundConfig(
+data class LegacySoundConfig(
+    @Serializable(SoundSerializer::class)
     var type: Sound,
     var volume: Float,
     var pitch: Float
